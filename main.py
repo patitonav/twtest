0ead3474505e593c71d81e494c6085462850591a
patitonav <pato.lalala@gmail.com>
2017-06-03 18:48:30 -0300

    Config file added

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
 # -*- coding: utf-8 -*-
 
 import tweepy, time, sys
+from config import CONSUMER_KEY, CONSUMER_SECRET, ACCESS_KEY, ACCESS_SECRET
+
 
 argfile = str(sys.argv[1])
 
 #enter the corresponding information from your Twitter application:
-CONSUMER_KEY = 'DeCNY09i3R5qTLfz65BlpA'	#keep the quotes, replace this with your consumer key
-CONSUMER_SECRET = 'hWwSjUa1iQXDRGAVzuaa1gXsTtiTY9S3y0JXtxPwYSQ'	#keep the quotes, replace this with your consumer secret key
-ACCESS_KEY = '1926951986-2ZogpaXC8FM4h8tBtx8784xeozA4GqNbAwjmysu'	#keep the quotes, replace this with your access token
-ACCESS_SECRET = 'NepKUpPCNl35fUqTwPWWSSIf5Zagvu6vItfqBlsMw'	#keep the quotes, replace this with your access token secret
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 api = tweepy.API(auth)
